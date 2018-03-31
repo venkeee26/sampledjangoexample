b87687c6dc62a96f9327af8351a03c9a5dbf4b9e
venkeee26 <34214238+venkeee26@users.noreply.github.com>
2018-03-31 13:49:13 -0500

    second commit

--- djangolevelfive/djangolevelfive/urls.py
+++ djangolevelfive/djangolevelfive/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
 from django.conf.urls import url,include
 from djangolevelfive_app import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^base/',include('djangolevelfive_app.urls')),
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     url(r'^logout$',views.user_logout,name='logout' )
 ]
